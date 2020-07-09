f55dd65d3ce89687e610f070aabd7e085312498a
deep <deepanshusehgal.112@gmail.com>
2020-07-09 16:59:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        {
+                WebView browser = findViewById(R.id.deepWeb);
+                browser.setWebViewClient(new WebViewClient());
+                browser.loadUrl("https://google.com");
+
+        }
+    }
+}
